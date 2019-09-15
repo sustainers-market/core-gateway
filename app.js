f737706d72d2089e1819f658f57cd04a538544a5
Joao <ritterjv@gmail.com>
2019-09-14 19:14:59 -0700

    -

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post(
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
         { maxAge: 86400000, httpOnly: true, secure: true }
       )
-      .send();
+      .send("🥑");
   })
 );
 
