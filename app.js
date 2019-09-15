3a8c779bd84d8128018870b982e31f87c80c58f0
Joao <ritterjv@gmail.com>
2019-09-14 18:31:22 -0700

    -

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.post(
       .cookie(
         "token",
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
-        { maxAge: 86400, httpOnly: true, secure: true }
+        { maxAge: 86400000, httpOnly: true, secure: true }
       )
       .send();
   })
